9d1b288f712095eb2fcc1dd78f7a6cf87e36f0f3
Linh Nguyen <76649421+LinhNguyen-97@users.noreply.github.com>
2025-10-30 10:47:39 +0700

    Create dLocation.sql

---
+++ dLocation.sql
@@ -0,0 +1,14 @@
+SELECT
+  DISTINCT City,
+  State,
+  Country,
+  Region,
+  Market,
+  CASE
+    WHEN State IN ("Alberta", "Manitoba", "Saskatchewan", "British Columbia") AND Region = "Canada" THEN "Western Canada"
+    WHEN Region = "Canada" THEN "Eastern Canada"
+    ELSE Region
+END
+  AS Region_use
+FROM
+  global-superstore-475905.global_superstore_rawdata.Orders
