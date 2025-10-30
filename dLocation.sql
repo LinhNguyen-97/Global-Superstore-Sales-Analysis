237090f086a83b94d8fd1a26f813328b20e79a6b
Linh Nguyen <76649421+LinhNguyen-97@users.noreply.github.com>
2025-10-30 16:09:12 +0700

    Update dLocation.sql

--- dLocation.sql
+++ dLocation.sql
@@ -1,14 +1,22 @@
+WITH
+  Trans_Location AS(
+  SELECT
+    DISTINCT City,
+    State,
+    Country,
+    Region,
+    Market,
+    CASE
+      WHEN State IN ("Alberta", "Manitoba", "Saskatchewan", "British Columbia") AND Region = "Canada" THEN "Western Canada"
+      WHEN Region = "Canada" THEN "Eastern Canada"
+      ELSE Region
+  END
+    AS Region_use
+  FROM
+    global-superstore-475905.global_superstore_rawdata.Orders )
+
 SELECT
-  DISTINCT City,
-  State,
-  Country,
-  Region,
-  Market,
-  CASE
-    WHEN State IN ("Alberta", "Manitoba", "Saskatchewan", "British Columbia") AND Region = "Canada" THEN "Western Canada"
-    WHEN Region = "Canada" THEN "Eastern Canada"
-    ELSE Region
-END
-  AS Region_use
+  ROW_NUMBER() OVER (ORDER BY City, State, Country, Region, Market, Region_use ) AS LocationID,
+  *
 FROM
-  global-superstore-475905.global_superstore_rawdata.Orders
+  Trans_Location
